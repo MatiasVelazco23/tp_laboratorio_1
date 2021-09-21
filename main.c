1e557c1e9eacf5263c07c9e936b214eda7d2506a
MatiasVelazco23 <matiasvelazco23@gmail.com>
2021-09-21 03:11:03 -0300

    menú casi listo, solo falta validar cuando se ingresa un string //////// falta hacer lo mismo en ingresar operandos, y también agregar que no se puedan meter caracteres ahí //////// falta arreglar la función dividir, pero la idea ya está, falta función multiplicar y la de ambos factoriales// me quedé en la clase 5/// falta pasar a eclipse y pasar las funcioneas y los headers a los archivos

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /*
 1
 Enunciado
@@ -39,6 +40,8 @@ hacer uso de dichas funciones para resolver la lógica del programa.
 //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   token github
 
 float calculoSuma(float n1, float n2);
+float calculoResta(float n1, float n2);
+float calculoDivision(float n1, float n2);
 
 int main()
 {
@@ -53,24 +56,22 @@ int main()
     float resultadoMultiplicacion;
     int resultadoFactorialA;
     int resultadoFactorialB;
-
-    //variable para seleccionar los casos del switch para las 5/6 opciones
-    int opciones;
-
-    /*flags para el ingreso de operadores y error de no-número en case1-case2
+     /*flags para el ingreso de operadores y error de no-número en case1-case2
     no se ingreso operador para el case3 todavía no hice lo del case1 y case2
     */
     int flagIngresoOperador1=0;
     int flagIngresoOperador2=0;
     int flagNoEsUnNumero=0;
     int flagNoSeIngresoOperador=0;
-
-    //flag para ingreso de opción incorrecta
-    int flagOpcInc=0;
-
+    int flagSeCalculo=0;
+    int flagSegundoOperador=0;
     //flag para salir del programa
     int flagSalir=0;
-
+    //flag para ingreso de opción incorrecta
+    int flagOpcInc=0;
+    //variable para seleccionar los casos del switch para las 5/6 opciones
+    char opciones;
+    char seguir= 's';
 
     // dowhile para volver a pedir menú
 
@@ -81,15 +82,19 @@ int main()
         {
             printf("1-Ingresar 1er operando (A=x)\n");
         }
+
         else if(operador1-(int)operador1!=0)
         {
             printf("1-Ingrese 1er operando (A=%.2f)\n", operador1);
         }
+
+
         else
         {
             printf("1-Ingrese 1er operando (A=%.0f)\n", operador1);
-
         }
+
+
         if(flagIngresoOperador2==0)
         {
             printf("2-Ingresar 2do operando (B=x)\n");
@@ -111,123 +116,213 @@ int main()
         //do while para pedir opción válida
         do
         {
-            if(flagOpcInc==1  ||opciones<1 && opciones>5)
+            if(flagOpcInc==1)
             {
-                printf("\nError: Esta no es una opción válida.\n\n");
-                printf("Recuerde ingresar una opción entre los números 1 y 5: \n");
+                printf("\nError: Esta no es una opción válida.\n");
+                printf("Recuerde ingresar una opción entre los números 1 y 5: ");
                 __fpurge(stdin);
-                scanf("%d", &opciones);
+
+                opciones=getchar();
+                printf("\n");
                 flagOpcInc=0;
+
             }
 
             else
             {
-                printf("Ingrese a continuación el número de la opción que eligió: \n ");
+                printf("Ingrese a continuación el número de la opción que eligió: ");
                 __fpurge(stdin);
-                scanf("%d", &opciones);
+                opciones=getchar();
+                printf("\n");
+                flagOpcInc=0;
 
             }
             switch(opciones)
             {
-            case 1:
+            case '1':
 
-                printf("Ingrese 1er operando: \n\n");
-                __fpurge(stdin);
+                printf("Ingrese 1er operando: ");
                 scanf("%f", &operador1);
+                printf("\n ");
                 flagIngresoOperador1=1;
 
                 break;
-            case 2:
-            do{
-                printf("Ingrese 2do operando: \n\n");
+            case '2':
+                printf("Ingrese 2do operando: ");
+                __fpurge(stdin);
                 scanf("%f", &operador2);
+                printf("\n ");
                 flagIngresoOperador2=1;
-                printf("\n");
-                if(isdigit((char)operador1))
-                {
-                printf("Esto no es un número");
-                flagNoEsUnNumero=1;
-                }
-                }while(flagNoEsUnNumero==1);
+                flagOpcInc=0;
                 break;
-            case 3:
+            case '3':
+            do{
                 resultadoSuma= calculoSuma(operador1,operador2);
+                resultadoResta= calculoResta(operador1,operador2);
+                resultadoDivision= calculoDivision(operador1,operador2);
                 /*calcResta=
                 calcDivision=
                 calcMultiplicacion=
                 calcFactA=
                 calcFactB=
                 */
+                flagSeCalculo=1;
                 printf("\nSe han realizado todos los cálculos posibles.\n\n");
+                printf("¿Deseas volver al menú? Ingresa 's' ");
+                __fpurge(stdin);
+                seguir=getchar();
+                }while(seguir!='s');
                 break;
-            case 4:
+            case '4':
+            do{
                 printf("Los resultado son: \n");
                 //mostrar suma
-                if(flagIngresoOperador1==0 && flagIngresoOperador2==1){
-                flagNoSeIngresoOperador=1;
+                if(flagIngresoOperador1==0 && flagIngresoOperador2==1)
+                {
+                    flagNoSeIngresoOperador=1;
                 }
-                else if(flagIngresoOperador1==1 && flagIngresoOperador2==0){
-                flagNoSeIngresoOperador=2;
+                else if(flagIngresoOperador1==1 && flagIngresoOperador2==0)
+                {
+                    flagNoSeIngresoOperador=2;
+                }
+                else if(flagIngresoOperador1==0 && flagIngresoOperador2==0)
+                {
+                    flagNoSeIngresoOperador=3;
+                }
+                else if(flagSeCalculo==0){
+                    flagNoSeIngresoOperador=4;
                 }
                 else{
-                flagNoSeIngresoOperador=3;
+                    flagNoSeIngresoOperador=0;
                 }
-                switch(flagNoSeIngresoOperador){
+                switch(flagNoSeIngresoOperador)
+                {
                 case 1:
-                printf("a) Falta el 1er operador para poder realizar la suma.\n");
-                break;
+                    printf("a)Falta el 1er operador para poder realizar la suma, vuelva al menú y seleccione la opción 1.\n");
+                    printf("b)Falta el 1er operador para poder realizar la resta, vuelva al menú y seleccione la opción 1.\n");
+
+                    flagNoSeIngresoOperador=0;
+                    break;
 
                 case 2:
-                printf("a) Falta el 2do operador para poder realizar la suma.\n");
-                break;
+                    printf("a)Falta el 2do operador para poder realizar la suma, vuelva al menú y seleccione la opción 2.\n");
+                    printf("a)Falta el 2do operador para poder realizar la resta, vuelva al menú y seleccione la opción 2.\n");
+
+                    flagNoSeIngresoOperador=0;
+                    break;
 
                 case 3:
-                printf("a) Faltan ambos operadores para realizar la suma\n");
-                break;
+                    printf("a)Faltan ambos operadores para realizar la suma, vuelva al menú y selecciónelos\n");
+                    printf("a)Faltan ambos operadores para realizar la resta, vuelva al menú y selecciónelos\n");
+                    flagNoSeIngresoOperador=0;
+                    break;
+                case 4:
+                   printf("a)No se calculó la suma, vuelve al menú y selecciona la opción 3\n");
+                   printf("b)No se calculó la resta, vuelve al menú y selecciona la opción 3\n");
+
+                   break;
                 default:
-                if((operador1 -(int)operador1==0) && (operador2-(int)operador2==0))
-                    printf("a)El resultado de %.0f + %.0f es: %.0f\n", operador1,operador2, resultadoSuma);
-                else if((((operador1 -(int)operador1!=0) && (operador2-(int)operador2!=0))) && (resultadoSuma - (int)resultadoSuma==0))
-                {
-                    printf("a)El resultado de %.2f + %.2f es: %.0f\n", operador1,operador2, resultadoSuma);
+                    if((operador1 -(int)operador1==0) && (operador2-(int)operador2==0))
+                    {
+                        printf("a)El resultado de %.0f + %.0f es: %.0f\n", operador1,operador2, resultadoSuma);
+                    }
+                     else if((((operador1 -(int)operador1!=0) && (operador2-(int)operador2!=0))) && (resultadoSuma - (int)resultadoSuma==0))
+                    {
+                        printf("a)El resultado de %.2f + %.2f es: %.0f\n", operador1,operador2, resultadoSuma);
+                    }
+
+
+                    else if((operador1-(int)operador1!=0) && (operador2-(int)operador2==0))
+                    {
+                        printf("a)El resultado de %.2f + %.0f es: %.2f\n", operador1,operador2, resultadoSuma);
+                    }
+                    else if((operador1-(int)operador1==0) && (operador2-(int)operador2!=0) )
+                    {
+                        printf("a)El resultado de %.0f + %.2f es: %.2f\n", operador1,operador2, resultadoSuma);
+                    }
+                    else
+                    {
+                        printf("a)El resultado de %.2f + %.2f es: %.2f\n", operador1,operador2, resultadoSuma);
+                    }
+                    //mostrar resta
+                    if((operador1 -(int)operador1==0) && (operador2-(int)operador2==0))
+                    {
+                        printf("a)El resultado de %.0f - %.0f es: %.0f\n", operador1,operador2, resultadoResta);
+                    }
+                     else if((((operador1 -(int)operador1!=0) && (operador2-(int)operador2!=0))) && (resultadoResta - (int)resultadoResta==0))
+                    {
+                        printf("a)El resultado de %.2f - %.2f es: %.0f\n", operador1,operador2, resultadoResta);
+                    }
+
+
+                    else if((operador1-(int)operador1!=0) && (operador2-(int)operador2==0))
+                    {
+                        printf("a)El resultado de %.2f - %.0f es: %.2f\n", operador1,operador2, resultadoResta);
+                    }
+                    else if((operador1-(int)operador1==0) && (operador2-(int)operador2!=0) )
+                    {
+                        printf("a)El resultado de %.0f - %.2f es: %.2f\n", operador1,operador2, resultadoResta);
+                    }
+                    else
+                    {
+                        printf("a)El resultado de %.2f - %.2f es: %.2f\n", operador1,operador2, resultadoResta);
+                    }
+                    //mostrar división
+                    switch(){
+                    case 0 :
+                    printf("c)No es posible dividir por cero\n");
+                    break;
+                    default :
+                    if((operador1 -(int)operador1==0) && (operador2-(int)operador2==0))
+                    {
+                        printf("a)El resultado de %.0f / %.0f es: %.0f\n", operador1,operador2, resultadoDivision);
+                    }
+                     else if((((operador1 -(int)operador1!=0) && (operador2-(int)operador2!=0))) && (resultadoDivision - (int)resultadoDivision==0))
+                    {
+                        printf("a)El resultado de %.2f / %.2f es: %.0f\n", operador1,operador2, resultadoDivision);
+                    }
+
+
+                    else if((operador1-(int)operador1!=0) && (operador2-(int)operador2==0))
+                    {
+                        printf("a)El resultado de %.2f / %.0f es: %.2f\n", operador1,operador2, resultadoDivision);
+                    }
+                    else if((operador1-(int)operador1==0) && (operador2-(int)operador2!=0) )
+                    {
+                        printf("a)El resultado de %.0f / %.2f es: %.2f\n", operador1,operador2, resultadoDivision);
+                    }
+                    else
+                    {
+                        printf("a)El resultado de %.2f - %.2f es: %.2f\n", operador1,operador2, resultadoDivision);
+                    }
+
+
+                    }
+
+
+
                 }
 
 
-                if((operador1-(int)operador1==0) && (operador2-(int)operador2==0))
-                {
-                    printf("a)El resultado de %.2f + %.0f es: %.2f\n", operador1,operador2, resultadoSuma);
-                }
-                if((operador1-(int)operador1!=0) && (operador2-(int)operador2==0))
-                {
-                    printf("a)El resultado de %.2f + %.0f es: %.2f\n", operador1,operador2, resultadoSuma);
-                }
-                if((operador2-(int)operador2!=0) && (operador1-(int)operador1==0))
-                {
-                    printf("a)El resultado de %.0f + %.2f es: %.2f\n", operador1,operador2, resultadoSuma);
-                }
-                }
-
-                //mostrar resta
-                printf("b)El resultado de A-B es: r\n"); // %d %d es %d op1,op2,resultadoResta
-                printf("c)El resultado de A/B es: r\n"); // %d %d es %f op1,op2,resultadoDivision
-                //o
-                printf("c)No es posible dividir por cero\n"); //????
                 printf("d)El resultado de A*B es: \n");// %d %d es %d op1,op2,resultadoMultiplicacion
                 printf("e)El factorial de A es: r1 y El factorial de B es: r2\n\n"); // %d es %d y %d es %d op1,resultadoFactorialA, op2, resultadoFactorialB
+
+                printf("¿Deseas volver al menú? Ingresa 's' ");
+                __fpurge(stdin);
+                seguir=getchar();
+                }while(seguir!='s');
                 break;
-            case 5:
+            case '5':
                 printf("Usted ha decidido salir del programa");
                 flagSalir=1;
                 break;
             default:
                 flagOpcInc=1;
+                break;
             }
         }while(flagOpcInc==1);
     }while(flagSalir==0);
-    //switch
 
-
-    //INFORMAR RESULTADOs
     return 0;
 
 }
@@ -238,5 +333,28 @@ float calculoSuma(float n1, float n2)
     resultadoSuma=n1+n2;
 
     return resultadoSuma;
+}
+
+
+float calculoResta(float n1, float n2)
+{
+    float resultadoResta;
+
+    resultadoResta=n1-n2;
+
+    return resultadoResta;
+}
+
+float calculoDivision(float n1, float n2 )
+{
+    int resultadoDivision;
+    if(n2==0){
+    }
+    else{
+    resultadoDivision=n1/n2;
+
+    return resultadoDivision;
+    }
+
 }
 
